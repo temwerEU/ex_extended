9be2ee8895184038b0b18d8878c8beda1230d801
thenexskk <thenexsk@protonmail.com>
2023-04-28 17:28:41 +0200

    prečo esx.. Prečo..

--- es_extended/server/commands.lua
+++ es_extended/server/commands.lua
@@ -82,9 +82,8 @@ end, false, {help = _U('command_cardel'), validate = false, arguments = {
 }})
 
 ESX.RegisterCommand('setaccountmoney', 'developer', function(xPlayer, args, showError)
-	print(xPlayer)
-	print(args.playerId)
-	print(json.ecnode(args.playerId))
+	print(json.encode(xPlayer))
+	print(json.encode(args.playerId))
 	if args.playerId.getAccount(args.account) then
 		args.playerId.setAccountMoney(args.account, args.amount, "Government Grant")
 	else
